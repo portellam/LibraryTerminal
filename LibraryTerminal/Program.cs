e64522037a911ed85ac48577daf6b4fb7d29f55a
portellam <amportell.13@gmail.com>
2021-12-07 09:19:47 -0500

    finished methods Read, WriteFile

--- LibraryTerminal/Program.cs
+++ LibraryTerminal/Program.cs
@@ -56,29 +56,13 @@ namespace LibraryTerminal
 	}
 	class Program
 	{
-		//Display the entire list of books. Format it nicely.
-		//static void ListBooks(List<Book> BookList)
-		//{ 
-		
-		
-		//}
-
 		//Search for a book by author.
-
-
-
-		//Search for a book by title keyword.
-
-
 
 		//Select a book from the list to check out.
 
-
 		//	-If it’s already checked out, let them know.
 
-
 		//	-If not, check it out to them and set the due date to 2 weeks from today. (The DateTime class will be helpful)
-
 
 		//Return a book.
 
@@ -109,97 +93,130 @@ namespace LibraryTerminal
 
 		static void ReadFile(List<Book> BookList)
 		{
-			//TODO: ask user if they wish to read from file, if not read from BookList in main
-
-			string line;
-
-			try
+			string input, line;
+			//ask user to read from file
+			Console.WriteLine("Do you wish to read the Library Inventory from file? (Y)es or (N)o:\t");
+			while (true)
 			{
-				//Read from file
-				StreamReader sr = new StreamReader("BookList.txt");
-
-				//Continue to read until you reach end of file
-				//Read the first line of text
-				while ((line = sr.ReadLine()) != null)
+				input = Console.ReadLine();
+				input = input.ToUpper().Substring(0);
+				if (input == "Y")
 				{
-					//TODO: implement write to file, each quality of class Book in BookList
-					string[] info = line.Split('\t');
-					Book aBook = new Book(info[0], info[1], bool.Parse(info[2]), int.Parse(info[3]));
-					//add aBook to BookList
-					Book.BookList.Add(aBook);
-					//Read the next line
-					line = sr.ReadLine();
+					try
+					{
+						//Read from file
+						StreamReader sr = new StreamReader("BookList.txt");
+
+						//Continue to read until you reach end of file
+						//Read the first line of text
+						while ((line = sr.ReadLine()) != null)
+						{
+							//TODO: implement write to file, each quality of class Book in BookList
+							string[] info = line.Split('\t');
+							Book aBook = new Book(info[0], info[1], bool.Parse(info[2]), int.Parse(info[3]));
+							//add aBook to BookList
+							Book.BookList.Add(aBook);
+							//Read the next line
+							line = sr.ReadLine();
+						}
+						//close the file
+						sr.Close();
+						Console.ReadLine();
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
 				}
-				//close the file
-				sr.Close();
-				Console.ReadLine();
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
+				else if (input == "N")
+				{
+					return;
+				}
+				else
+				{
+					Console.WriteLine("(Y)es or (N)o:\t");
+				}
 			}
 		}
 
 		static void WriteFile(List<Book> BookList)
 		{
 			//TODO: ask user if they wish to write to file
+			string input;
 
-			try
+			Console.WriteLine("Do you wish to read the Library Inventory from file? (Y)es or (N)o:\t");
+			while (true)
 			{
-				//Create or update file
-				StreamWriter sw = new StreamWriter("BookList.txt");
-				//Write BookList to file 
-				for (int index = 0; index < BookList.Count; index++)
+				input = Console.ReadLine();
+				input = input.ToUpper().Substring(0);
+				if (input == "Y")
 				{
-					Book aBook = BookList[index];
-					//write aBook to next line
-					sw.WriteLine(aBook.ToString());
+					try
+					{
+						//Create or update file
+						StreamWriter sw = new StreamWriter("BookList.txt");
+						//Write BookList to file 
+						for (int index = 0; index < BookList.Count; index++)
+						{
+							Book aBook = BookList[index];
+							//write aBook to next line
+							sw.WriteLine(aBook.ToString());
+						}
+						//Close the file
+						sw.Close();
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
 				}
-				//Close the file
-				sw.Close();
+				else if (input == "N")
+				{
+					return;
+				}
+				else
+				{
+					Console.WriteLine("(Y)es or (N)o:\t");
+				}
 			}
-			catch (Exception ex)
+
+			static void Main(string[] args)
 			{
-				Console.WriteLine(ex.Message);
+
+				//TODO: implement to read/write from/to text file, to save BookList for later runs
+				//	read from text file
+				//ReadFile(BookList);
+				//
+
+				Book.BookList.Add(new Book("Green Eggs and Ham", "Dr. Seuss", true, 0));
+				Book.BookList.Add(new Book("The Art of War", "Sun Tzu", true, 0));
+				Book.BookList.Add(new Book("Queenie", "Candice Carty-Williams", false, 14));
+				Book.BookList.Add(new Book("The 48 Laws of Power", "Robert Greene", false, 12));
+				Book.BookList.Add(new Book("The Alchemist", "Paulo Coelho", true, 0));
+				Book.BookList.Add(new Book("Honey Girl", "Morgan Rogers", false, 6));
+				Book.BookList.Add(new Book("A History of Central Banking", "Stephen Mittford Goodson", false, 7));
+				Book.BookList.Add(new Book("Children of Blood and Bone", "Tomi Adeyemi", true, 0));
+				Book.BookList.Add(new Book("The Hate U Give", "Angie Thomas", true, 0));
+				Book.BookList.Add(new Book("Twilight", "Stephenie Meyer", false, 2));
+				Book.BookList.Add(new Book("To Kill a Mockingbird", "Harper Lee", true, 0));
+				Book.BookList.Add(new Book("Leading with My Chin", "Jay Leno", false, 9));
+
+				//TODO: implement main loop
+				//	main code here
+				//do
+				//{
+				//	begin code here
+
+				//	end code here
+				//} while (ContinueProgram() == true);
+				//			
+
+				//TODO: implement write to text file, contents of BookList
+				//	read from text file
+				//WriteFile(BookList);
+				//
+				//	end program here
 			}
-		}
-
-		static void Main(string[] args)
-		{
-
-			//TODO: implement to read/write from/to text file, to save BookList for later runs
-			//	read from text file
-			//ReadFile(BookList);
-			//
-			
-			Book.BookList.Add(new Book("Green Eggs and Ham", "Dr. Seuss", true, 0));
-			Book.BookList.Add(new Book("The Art of War", "Sun Tzu", true, 0));
-			Book.BookList.Add(new Book("Queenie", "Candice Carty-Williams", false, 14));
-			Book.BookList.Add(new Book("The 48 Laws of Power", "Robert Greene", false, 12));
-			Book.BookList.Add(new Book("The Alchemist", "Paulo Coelho", true, 0));
-			Book.BookList.Add(new Book("Honey Girl", "Morgan Rogers", false, 6));
-			Book.BookList.Add(new Book("A History of Central Banking", "Stephen Mittford Goodson", false, 7));
-			Book.BookList.Add(new Book("Children of Blood and Bone", "Tomi Adeyemi", true, 0));
-			Book.BookList.Add(new Book("The Hate U Give", "Angie Thomas", true, 0));
-			Book.BookList.Add(new Book("Twilight", "Stephenie Meyer", false, 2));
-			Book.BookList.Add(new Book("To Kill a Mockingbird", "Harper Lee", true, 0));
-			Book.BookList.Add(new Book("Leading with My Chin", "Jay Leno", false, 9));
-
-			//TODO: implement main loop
-			//	main code here
-			//do
-			//{
-			//	begin code here
-
-			//	end code here
-			//} while (ContinueProgram() == true);
-			//			
-
-			//TODO: implement write to text file, contents of BookList
-			//	read from text file
-			//WriteFile(BookList);
-			//
-			//	end program here
 		}
 	}
 }
