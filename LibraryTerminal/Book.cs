52d2fdcef9903e186573bf0ffa09f802f144e168
beluke <93033613+beluke@users.noreply.github.com>
2021-12-07 13:43:53 -0500

    add read/write and cleaned up functions

--- LibraryTerminal/Book.cs
+++ LibraryTerminal/Book.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -12,9 +12,10 @@ namespace LibraryTerminal
 		public string Title;
 		public string Author;
 		public bool Status;
-		public int DueDate;
+		public DateTime DueDate;
 
-		public Book(string title, string author, bool status, int dueDate)
+		/* CONSTRUCTORS */
+		public Book(string title, string author, bool status, DateTime dueDate)
 		{
 			Title = title;
 			Author = author;
@@ -22,6 +23,7 @@ namespace LibraryTerminal
 			DueDate = dueDate;
 		}
 
+		/* MEMBER METHODS */
 		public string BookStatus()
 		{
 			if (Status)
@@ -34,11 +36,33 @@ namespace LibraryTerminal
 			}
 		}
 
-		public override string ToString()
+		public bool ReturnBook()
 		{
-			return String.Format("{0}\t{1}\t{2}\t${3}\t", Title, Author, Status, DueDate);
+			// if book is already on shelf, it cannot be returned, return false (i.e. failure)
+			if (Status) return false;
+
+			// if book is not on shelf, invert status and return true (i.e. success)
+			Status = !Status;
+			return true;
 		}
 
+		public bool CheckOutBook()
+		{
+			// if book is not available, return false (i.e. failure)
+			if (!Status) return false;
+
+			// if book is available invert status, return true (i.e. success)
+			Status = !Status;
+			return true;
+		}
+
+		/* OVERRIDES */
+		public override string ToString()
+		{
+			return String.Format("{0,-40} {1,-35} {2,-15} {3,-11}", Title, Author, BookStatus(), DueDate.ToString("d"));
+		}
+
+		/* STATIC BOOK METHODS */
 		public static Book SearchAuthor(string input)
 		{
 			foreach (Book book in BookList)
@@ -65,37 +89,134 @@ namespace LibraryTerminal
 			return null;
 		}
 
-		public static string ListBook()
+		// list all books in BookList
+		public static string ListBooks()
 		{
 			string output = "";
-			Console.WriteLine($"#\tTITLE\tAUTHOR\tSTATUS\tDUE DATE (days)");
-			for (int index = 0; index < BookList.Count; index++)
+            Console.WriteLine(String.Format("{0,-40} {1,-35} {2,-15} {3,-11}", "Title", "Author", "Status", "Due Date"));
+            Console.WriteLine("{0,-40} {1,-35} {2,-15} {3,-11}", "=====", "======", "======", "========");
+            for (int index = 0; index < BookList.Count; index++)
 			{
 				Book aBook = BookList[index];
-				output += index + 1 + "\t" + aBook.ToString() + "\n";
+				output += aBook.ToString() + "\n";
 			}
 			return output;
 		}
 
-		public bool Return()
+		// moved to Book as a static method for initializing BookList from a file
+		public static bool ReadFile()
 		{
-			// if book is already on shelf, it cannot be returned, return false (i.e. failure)
-			if (Status) return false;
+			// input is unecessary here since we are not looping
+			string /*input,*/ line;
 
-			// if book is not on shelf, invert status and return true (i.e. success)
-			Status = !Status;
+			//no need to ask user if they want to read from a file inside ReadFile()
+			//Console.WriteLine("Do you wish to read the Library Inventory from file? (Y)es or (N)o:\t");
+			/*while (true)
+			{
+			input = Console.ReadLine();
+			input = input.ToUpper().Substring(0);
+			if (input == "Y")
+			{*/
+			// we just need try..catch since we are not looping inside of ReadFile()
+
+			// get file name from user
+			Console.Write("Enter filename: ");
+			string path = Console.ReadLine();
+
+			try
+			{
+				if (BookList.Count > 0) BookList.Clear();
+
+				//	Read from file
+				StreamReader sr = new StreamReader(path);
+				//	Continue to read until you reach end of file
+				//	Read the first line of text
+				while ((line = sr.ReadLine()) != null)
+				{
+					//	TODO: implement write to file, each quality of class Book in BookList
+					string[] info = line.Split('\t');
+					Book aBook = new Book(info[0], info[1], bool.Parse(info[2]), DateTime.Parse(info[3]));
+					//	add aBook to BookList
+					Book.BookList.Add(aBook);
+					//	Read the next line
+					line = sr.ReadLine();
+				}
+				//	close the file
+				sr.Close();
+				Console.ReadLine();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return false;
+			}
+
 			return true;
+			/*}
+			else if (input == "N")
+			{
+				return;
+			}
+			else
+			{
+				Console.WriteLine("(Y)es or (N)o:\t");
+			}*/
 		}
 
-		public bool CheckOut()
+		// moved to Book as a static method for saving BookList to a file
+		public static void WriteFile()
 		{
-			// if book is not available, return false (i.e. failure)
-			if (!Status) return false;
+			//	TODO: ask user if they wish to write to file
+			//string input;
 
-			// if book is available invert status, return true (i.e. success)
-			Status = !Status;
-			return true;
+			// line below is unecessary as we can ask the user if they want to save to a file before we call WriteFile()
+			//Console.WriteLine("Do you wish to read the Library Inventory from file? (Y)es or (N)o:\t");
+			/*while (true)
+			{*/
+			//input = Console.ReadLine();
+			//input = input.ToUpper().Substring(0);
+			/*if (input == "Y")
+			{
+			*/
+			// we just need the try catch block since we are not looping inside of WriteFile
+
+			// get file name from user
+			Console.Write("Enter filename: ");
+			string path = Console.ReadLine();
+
+			try
+			{
+				if (BookList.Count <= 0) throw new Exception("Cannot save an empty list of books...");
+
+				// using path parameter to control what the file is called and where it should be saved
+				StreamWriter sw = new StreamWriter(path);
+				//	Write BookList to file 
+				for (int index = 0; index < BookList.Count; index++)
+				{
+					Book aBook = BookList[index];
+					//	write aBook to next line
+					sw.WriteLine(aBook.ToString());
+				}
+				//	Close the file
+				sw.Close();
+			}
+			catch (Exception ex)
+			{
+				// updated error ouput message
+				Console.WriteLine($"Error writing to file: {ex.Message} (Path: {path})");
+			}
+			/*
+			}
+			
+			else if (input == "N")
+			{
+				return;
+			}
+			else
+			{
+				Console.WriteLine("(Y)es or (N)o:\t");
+			}*/
 		}
-
 	}
+
 }
